e10fb64840b8ffc02e9f80f075bf0e0f2901000f
Alana <rcclewu@gmail.com>
2025-05-13 10:04:38 -0700

    initial layout

--- lets-picnic/src/App.tsx
+++ lets-picnic/src/App.tsx
@@ -1,9 +1,11 @@
 import Categories from "./components/Categories";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <main className="bg-gray-100">
       <Categories />
+      <Nav />
     </main>
   );
 }
