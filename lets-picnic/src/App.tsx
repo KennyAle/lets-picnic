d419a2f0c5db1e29e05f6288a176e653faf2cc48
Alana <rcclewu@gmail.com>
2025-05-14 12:19:29 -0700

    initail lauout

--- lets-picnic/src/App.tsx
+++ lets-picnic/src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Categories from "./components/Categories";
 import DeliverAndMembership from "./components/DeliverAndMembership";
+import DiscountAndPromot from "./components/DiscountAndPromot";
 import Nav from "./components/Nav";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Banner />
       <Categories />
+      <DiscountAndPromot />
       <DeliverAndMembership />
     </main>
   );
