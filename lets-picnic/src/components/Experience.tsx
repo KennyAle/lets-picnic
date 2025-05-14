25f213c8aab5ee2428b2a9bb9b4487ca4375dd0d
KennyAle <kennyviver@gmail.com>
2025-05-14 10:41:30 -0700

    Experience Component

--- lets-picnic/src/components/Experience.tsx
+++ lets-picnic/src/components/Experience.tsx
@@ -2,28 +2,32 @@ const Info = [
   { name: "Order and Collect", image: "https://placehold.co/150x200" },
   { name: "Gift Vouchers", image: "https://placehold.co/150x200" },
   { name: "Present a Gift card", image: "https://placehold.co/150x200" },
-  { name: "Pay your invoice", image: "https://placehold.co/150x200" },
+  { name: "Pay your Invoice", image: "https://placehold.co/150x200" },
+  { name: "Pay your Invoice", image: "https://placehold.co/150x200" },
 ];
 
 const Experience = () => {
   return (
-    <div className="h-screen flex flex-col items-center  bg-lime-300">
-      <div className="h-full flex flex-col items-center justify-center text-green-900 p-10">
-        <h2>We always provide you the best in town</h2>
-        <p>
+    <div className="relative h-screen flex flex-col items-center bg-lime-300">
+      <div className="experience w-full h-full bg-white"></div>
+      <div className="h-1/2 text-center flex flex-col gap-5 items-center justify-center text-teal-950 p-10">
+        <h2 className="w-3/6 text-4xl font-bold tracking-tight">
+          We always provide
+          <br /> you the best in town
+        </h2>
+        <p className="w-4/7 text-sm font-semibold">
           Since 2007 we have been delivering excellence in product development,
           support & updates for frictionless shopping experiences
         </p>
       </div>
-
-      <ul className="flex gap-4 h-full">
+      <ul className="flex gap-8 h-1/2 w-full">
         {Info.map((item) => (
           <li
             key={item.name}
-            className="flex flex-col gap-2 w-1/4 bg-green-900"
+            className="round-shape-top flex flex-col justify-end gap-8 w-1/5 mx-auto px-5 pt-8 bg-teal-950 text-lime-200 font-semibold text-xl"
           >
-            <h2>{item.name}</h2>
-            <img src={item.image} className="w-1/2" alt="" />
+            <h2 className="self-start w-2/3">{item.name}</h2>
+            <img src={item.image} className="h-3/5" alt="" />
           </li>
         ))}
       </ul>
