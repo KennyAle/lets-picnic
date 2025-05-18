ea60da57e3853a045e186396bda4fbbcd8fd72a2
Alana <rcclewu@gmail.com>
2025-05-18 16:48:26 -0700

    added pictures, animations

--- lets-picnic/src/components/DiscountAndPromot.tsx
+++ lets-picnic/src/components/DiscountAndPromot.tsx
@@ -1,50 +1,70 @@
+import { motion } from "framer-motion";
+
+const container ={
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2,
+      ease: "easeOut"
+    },
+  },
+}
+const item = {
+  hidden: { opacity: 0, x: 30},
+  show: { opacity: 1, x: 0, transition: { duration:0.2, ease: "easeOut"}}
+}
+
 const DiscountAndPromot = () => {
   return (
-    <div className="flex justify-between gap-5">
-        <section className="flex flex-col justify-center w-3xs h-100 bg-pink-200 rounded-2xl">
+    <motion.div className="flex justify-between gap-5"
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+    >
+        <motion.section className="flex flex-col justify-center w-3xs h-100 bg-pink-200 rounded-2xl" variants={item}>
             <div className="h-1/2 flex flex-col gap-3 justify-center bg-pink-200 rounded-t-2xl px-7 pt-3">
                 <h2 className="text-rose-950 font-extrabold text-lg">Save</h2>
                 <h3 className="text-rose-800 font-extrabold text-3xl font-mono">$29</h3>
                 <p className="font-semibold">Enjoy Dicount all types of Grocery & frozen item</p>
             </div>
             <div className="h-1/2 flex justify-center items-center bg-rose-900 rounded-b-2xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-3/4 after:h-3/4 after:bg-rose-950 after:rounded-[70px_70px_70px_15px] round-shape-top">
-                <img className="z-10" src="https://placehold.jp/100x150.png" alt="" />
+                <img className="h-fit w-3/4 z-10" src="https://freepngimg.com/thumb/yummy/162136-chips-lays-png-download-free.png" alt="" />
             </div>
-        </section>
+        </motion.section>
 
-        <section  className="flex flex-col justify-center w-3xs h-100 bg-orange-200 rounded-2xl">
+        <motion.section  className="flex flex-col justify-center w-3xs h-100 bg-orange-200 rounded-2xl" variants={item}>
             <div className="h-1/2 flex flex-col gap-3 justify-center bg-orange-200 rounded-t-2xl px-7 pt-3">
                 <h2 className="text-orange-950 font-extrabold text-lg">Discount</h2>
                 <h3 className="text-orange-900 font-extrabold text-3xl font-mono">30%</h3>
                 <p className="font-semibold">Enjoy Dicount all types of Grocery & frozen item</p>
             </div>
             <div className="h-1/2 flex justify-center items-center bg-orange-800 rounded-b-2xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-3/4 after:h-3/4 after:bg-orange-900 after:rounded-[70px_70px_70px_15px] round-shape-top">
-                <img className="z-10" src="https://placehold.jp/100x150.png" alt="" />
+                <img className="h-fit w-2/4 z-10" src="https://freepngimg.com/thumb/honey/150509-honey-bottle-free-clipart-hq.png" alt="" />
             </div>
-        </section >
+        </motion.section >
 
-        <section className="flex flex-col justify-center w-3xs h-100 bg-blue-200 rounded-2xl">
+        <motion.section className="flex flex-col justify-center w-3xs h-100 bg-blue-200 rounded-2xl" variants={item}>
             <div className="h-1/2 flex flex-col gap-3 justify-center bg-blue-200 rounded-t-2xl px-7 pt-3">
                 <h2 className="text-blue-900 font-extrabold text-lg">Up tp</h2>
                 <h3 className="text-blue-800 font-extrabold text-3xl font-mono">50%</h3>
                 <p className="font-semibold">Enjoy Dicount all types of Grocery & frozen item</p>
             </div>
             <div className="h-1/2 flex justify-center items-center bg-blue-900 rounded-b-2xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-3/4 after:h-3/4 after:bg-blue-950 after:rounded-[70px_70px_70px_15px] round-shape-top">
-                <img className="z-10" src="https://placehold.jp/100x150.png" alt="" />
+                <img className="h-fit w-2/4 z-10" src="https://freepngimg.com/thumb/bread/170069-bagel-png-free-photo.png" alt="" />
             </div>
-        </section>
+        </motion.section>
 
-        <section className="flex flex-col justify-center w-3xs h-100 bg-purple-200 rounded-2xl">
+        <motion.section className="flex flex-col justify-center w-3xs h-100 bg-purple-200 rounded-2xl" variants={item}>
             <div className="h-1/2 flex flex-col gap-3 justify-center bg-purple-200 rounded-t-2xl px-7 pt-3">
                 <h2 className="text-purple-950 font-extrabold text-lg">Free</h2>
                 <h3 className="text-purple-900 font-extrabold text-3xl font-mono">SHIP</h3>
                 <p className="font-semibold">Enjoy Dicount all types of Grocery & frozen item</p>
             </div>
             <div className="h-1/2 flex justify-center items-center bg-purple-900 rounded-b-2xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-3/4 after:h-3/4 after:bg-purple-950 after:rounded-[70px_70px_70px_15px] round-shape-top">
-                <img className="z-10" src="https://placehold.jp/100x150.png" alt="" />
+                <img className="h-fit w-3/4 z-10" src="https://freepngimg.com/thumb/drink/135228-juice-vinegar-cider-apple-beet.png" alt="" />
             </div>
-        </section>
-    </div>
+        </motion.section>
+    </motion.div>
   )
 }
 
--- lets-picnic/src/components/Experience.tsx
+++ lets-picnic/src/components/Experience.tsx
@@ -1,36 +1,61 @@
+import { motion } from "framer-motion";
+
+const container ={
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2,
+      ease: "easeOut"
+    },
+  },
+}
+const item = {
+  hidden: { opacity: 0, y: 30},
+  show: { opacity: 1, y: 0, transition: { duration:0.3, ease: "easeOut"}}
+}
+
 const Info = [
-  { name: "Order and Collect", image: "https://placehold.co/150x200" },
-  { name: "Gift Vouchers", image: "https://placehold.co/150x200" },
-  { name: "Present a Gift card", image: "https://placehold.co/150x200" },
-  { name: "Pay your Invoice", image: "https://placehold.co/150x200" },
-  { name: "Pay your New", image: "https://placehold.co/150x200" },
+  { name: "Order and Collect", image: "images/img_order_collect.png" },
+  { name: "Gift Vouchers", image: "images/img_vouchers.png" },
+  { name: "Present a Gift card", image: "images/img_gift_card.png" },
+  { name: "Pay your Invoice", image: "images/img_pay_your_invoice.png" },
+  { name: "Pay your New", image: "images/img_order_collect.png" },
 ];
 
 const Experience = () => {
   return (
-    <div className="relative h-screen flex flex-col items-center bg-lime-300">
+    <div className="relative h-screen flex flex-col items-center bg-lime-300 overflow-hidden">
       <div className="experience w-full h-full bg-gray-100"></div>
-      <div className="h-1/2 text-center flex flex-col gap-5 items-center justify-center text-teal-950 p-10">
-        <h2 className="w-3/6 text-4xl font-bold tracking-tight">
+      <motion.div className="h-1/2 text-center flex flex-col gap-5 items-center justify-center text-teal-950 p-10"
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+      >
+        <motion.h2 className="w-3/6 text-4xl font-bold tracking-tight" variants={item}>
           We always provide
-          <br /> you the best in town
-        </h2>
-        <p className="w-4/7 text-sm font-semibold">
+          <motion.br variants={item}/> you the best in town
+        </motion.h2>
+        <motion.p className="w-4/7 text-sm font-semibold" variants={item}>
           Since 2007 we have been delivering excellence in product development,
           support & updates for frictionless shopping experiences
-        </p>
-      </div>
-      <ul className="flex gap-8 h-1/2 w-full">
-        {Info.map((item) => (
+        </motion.p>
+      </motion.div>
+      <motion.ul className="flex gap-8 h-1/2 w-max"
+        initial={{x:0}}
+        animate={{x:"-30%"}}
+        transition={{ repeat: Infinity, repeatType: "loop", duration: 30, ease: "linear" }}
+      >
+        {[...Info, ...Info].map((item, index) => (
           <li
-            key={item.name}
-            className="round-shape-top flex flex-col justify-end gap-8 w-1/5 mx-auto px-5 pt-8 bg-teal-950 text-lime-200 font-semibold text-xl"
+            key={index}
+            className="round-shape-top flex flex-col justify-end gap-8 w-1/5 h-fit mx-auto px-5 pt-8 bg-teal-950 text-lime-200 font-semibold text-xl"
           >
-            <h2 className="self-start w-2/3">{item.name}</h2>
-            <img src={item.image} className="h-3/5" alt="" />
+            <h2 className="self-start w-2/3 ">{item.name}</h2>
+            <img src={item.image} className="h-fit w-3/4 self-center" alt="" />
           </li>
         ))}
-      </ul>
+        
+      </motion.ul>
     </div>
   );
 };
