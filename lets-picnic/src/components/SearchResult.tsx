d29352ce6200f7a0e5cc8790e7aa7a18171135bd
KennyAle <kennyviver@gmail.com>
2025-05-20 03:54:47 -0700

    FIX Search Bar

--- lets-picnic/src/components/SearchResult.tsx
+++ lets-picnic/src/components/SearchResult.tsx
@@ -1,46 +1,53 @@
-import { type Product } from "../types/product.types"
-import {motion} from 'framer-motion'
+import { Link } from "react-router-dom";
+import { type Product } from "../types/product.types";
+import { motion } from "framer-motion";
 
 type ProductWrapper = {
   product: Product;
 };
 
 export interface ISearchResult {
-    searched: boolean,
-    searchResult: ProductWrapper[]
+  searched: boolean;
+  searchResult: ProductWrapper[];
 }
 
-const SearchResult = ({searched, searchResult}: ISearchResult) => {
-    
+const SearchResult = ({ searched, searchResult }: ISearchResult) => {
   return (
-    <motion.div className="fixed h-[calc(100vh-68px)] overflow-y-auto top-16 z-100 w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 bg-white"
-        initial={{opacity:0, y:"-100%"}}
-        animate={{opacity:1, y:0}}
-        transition={{
-            type: "spring",     
-            stiffness: 50,      
-            damping: 10,        
-            duration: 0.2,      
-  }}
+    <motion.div
+      className="flex flex-wrap fixed p-5 gap-6 overflow-auto bg-gray-200 top-16 z-100 w-full h-2/3 shadow-md"
+      initial={{ opacity: 0, y: "-100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 50,
+        damping: 10,
+        duration: 0.2,
+      }}
     >
-        {searched && (
-          searchResult.length > 0 ? (
-            searchResult.map(item => (
-            <div key={item.product.id} className="flex flex-col justify-center items-center p-4 rounded-lg">
-                <img src={item.product.image} alt={item.product.productName} className="w-1/2"/>
-                <h2 className="text-center text-green-900 font-semibold">{item.product.productName}</h2>
-                <p className="text-green-900 font-bold text-xl">${item.product.price.toFixed(2)}</p>
-            </div>
-            ))
-          ):(
-            <div className="flex w-screen justify-center items-center">
-                <img src="images/img_sorry.png" alt="" />
-            </div>
-            
-          )
-      )}
+      {searched &&
+        (searchResult.length > 0 ? (
+          searchResult.map((item) => (
+            <Link to={`/products/product/${item.product.id}`}
+              key={item.product.id}
+              className="w-40 bg-white flex flex-col justify-center items-center p-4 rounded-lg"
+            >
+              <img
+                src={item.product.image}
+                alt={item.product.productName}
+                className="w-40"
+              />
+              <h2 className="text-center text-green-900 font-semibold">
+                {item.product.productName}
+              </h2>
+            </Link>
+          ))
+        ) : (
+          <div className="flex w-screen justify-center items-center">
+            <img src="images/img_sorry.png" alt="" />
+          </div>
+        ))}
     </motion.div>
-  )
-}
+  );
+};
 
-export default SearchResult
+export default SearchResult;
